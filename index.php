f0ac7ae9cd5a80aa4f5792772c57dcb355ca9558
rlabou <rlabou@click-call.com>
2017-04-16 19:02:58 +0200

    modification 1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
 echo 'Page index';
 echo '<br>';
-echo 'Je suis un texte de la version de prod actuellement 22h40';
+echo 'Je suis un texte de la version de prod actuellement 16-04-2017 19h01';
 ?>
--- page1.php
+++ page1.php
@@ -1,5 +1,5 @@
 <?
 
-echo 'Je suis la page 1';
+echo 'Je suis la page 1   - 16-04-2017:19h02';
 
 ?>
